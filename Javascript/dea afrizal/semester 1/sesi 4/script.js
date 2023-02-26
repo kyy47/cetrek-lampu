4fedd3d2f3dcaceaf35fb03f7a72ff9d57cc23c7
kyy47 <ourbaerapy@gmail.com>
2023-02-26 17:02:02 +0800

    test

--- Javascript/dea afrizal/semester 1/sesi 4/script.js
+++
@@ -1,9 +0,0 @@
-function nyalakanLampu(){
-    document.getElementById("lampu-off").style.display = "none"
-    const on = document.createElement("img")
-    on.src = "./assets/img/on.png"
-    const cetrekan = document.getElementById("cetrekan-lampu")
-    cetrekan.before(on)
-    cetrekan.textContent = "LAMPU NYALA!"
-    document.getElementById("button-cetrekan").style.display = "none"
-}
